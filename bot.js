885286d7d8df10ac8e2d80f167f795e0e385acfa
LoudSoftware <nicoznam@gmail.com>
2018-04-06 15:27:51 -0400

    Added initial files

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
-var Discord = require('discord.io');
+var eris = require('eris');
 var logger = require('winston');
 
 // Loading the token from the .env file
 var dotenv = require('dotenv').config();
+
+token = process.env.BOT_TOKEN;
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -11,36 +13,3 @@ logger.add(logger.transports.Console, {
 });
 logger.level = 'debug';
 // Initialize Discord Bot
-var bot = new Discord.Client({
-   token: process.env.BOT_TOKEN,
-   autorun: true
-});
-
-// When bot gets connected, this happens
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-
-
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '#') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
