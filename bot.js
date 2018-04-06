b8fa755ad688d1e63abc246062080beb933f63a7
LoudSoftware <nicoznam@gmail.com>
2018-04-06 16:13:49 -0400

    Added basic integer detection and characer shaming

--- bot.js
+++ bot.js
@@ -6,21 +6,44 @@ var dotenv = require('dotenv').config();
 
 token = process.env.BOT_TOKEN;
 
+const channelID = "243191930840809473";
+const botID = "431866126851506177";
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-// Initialize Discord Bot
 
-var bot = new Eris(process.env.BOT_TOKEN);
-bot.on("ready", () => {
-    console.log("Ready!");
+
+console.log('Attempting to connect to server...');
+
+var bot = new Eris.CommandClient(process.env.BOT_TOKEN, {}, {
+    description: "Description",
+    owner: "@LoudSoftware",
+    prefix: "#",
 });
-bot.on("messageCreate", (msg) => {
-    if(msg.content === "#ping") {
-        bot.createMessage(msg.channel.id, "Pong!");
+
+bot.registerCommand("ping", "Pong!",{
+    description: "pong",
+    fullDescription: "does pong",
+
+});
+
+bot.on("ready", () => {
+    console.log('Bot ready');
+});
+
+bot.on("messageCreate",(msg) => {
+    if (msg.channel.id == channelID){
+        if(isNaN(msg.content) && msg.author.id != botID){
+            msg.delete("No characters!!");
+            
+            bot.createMessage(channelID, `Shame... ${msg.author.mention} wrote something that was not a number`);
+        }
     }
 });
+
+
 bot.connect();
